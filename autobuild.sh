5caaf0f509f9a099b59bc8c03d527476bfe98e2d
saitamasahil <100558482+saitamasahil@users.noreply.github.com>
2022-12-05 12:26:39 +0530

    if zip tool isn't found then exit

--- autobuild.sh
+++ autobuild.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
 # Setup script
 
-# Install zip
-apt-get install zip
+# Install zip if it is not already installed
+if ! command -v zip >/dev/null; then
+  echo "Error: zip is not installed. Please install it manually and try again."
+  exit 1
+fi
 
 # Create directory if it doesn't exist
 mkdir -p "$HOME/Pixel-Launcher-Extended"
