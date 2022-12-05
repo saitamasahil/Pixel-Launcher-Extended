8b44bf7368b082e082d3c7240166a14bd20989c9
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2022-12-05 12:54:04 +0530

    Fixed directory issues in script

--- autobuild.sh
+++ autobuild.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/bash
 # Setup script
 
-# Install zip if it is not already installed
 if ! command -v zip >/dev/null; then
   echo "Error: zip is not installed. Please install it manually and try again."
   exit 1
 fi
 
-# Create directory if it doesn't exist
-mkdir -p "$HOME/Pixel-Launcher-Extended"
-
-# Make sure the script has the correct permissions to access the directory
-chmod a+rwx "$HOME/Pixel-Launcher-Extended"
-
-# Change to the /tmp directory
-cd /tmp
+# Check if the current directory has system folder and setup.sh to verify that current directory is valid
+if [ ! -d "system" ] || [ ! -f "setup.sh" ]; then
+  echo "Error: Current directory is not valid. Make sure that you are in the right directory and try again."
+  exit 1
+fi
 
 # Create zip file
 echo ">> Creating zip file"
-zip -r PixelLauncherExtended.zip "$HOME/Pixel-Launcher-Extended"
-echo ">> Done! You can find the zip file in the /tmp directory - /tmp/PixelLauncherExtended.zip"
+echo "" # make the output look easier to read
+zip -r PixelLauncherExtended.zip . -x .git/\* # Ignore .git folder because it's not needed for the module
+echo "" # make the output look easier to read
+echo ">> Done! You can find the zip file in the current directory - '$(pwd)/PixelLauncherExtended.zip'"
