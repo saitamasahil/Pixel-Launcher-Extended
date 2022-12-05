a3bace248c3f5ba3876afddb2be8ac850352a716
saitamasahil <sahilmanchanda1996@gmail.com>
2022-12-05 10:55:32 +0530

    updated autobuild.sh

--- autobuild.sh
+++ autobuild.sh
@@ -1,18 +1,25 @@
-if ! command -v zip &> /dev/null
-then
-    echo "'zip' could not be found"
-    echo "Please install zip"
+#!/usr/bin/bash
+# Setup script
+
+# Install requirements
+sudo apt-get install zip
+
+# Make autobuild.sh executable
+chmod +x autobuild.sh
+
+# Give permission
+sudo ./autobuild.sh
+
+# Check if the Pixel-Launcher-Extended directory exists and contains files
+if [ ! -d Pixel-Launcher-Extended ] || [ -z "$(ls -A Pixel-Launcher-Extended)" ]; then
+    echo ">> Pixel-Launcher-Extended directory not found or is empty"
     exit
+else
+    # Make sure the script has the correct permissions to access the directory
+    chmod u+rwx Pixel-Launcher-Extended
+
+    # Create zip file
+    echo ">> Creating zip file"
+    zip -r PixelLauncherExtended.zip Pixel-Launcher-Extended
+    echo ">> Done! You can find the zip file in the current directory - $(pwd)/PixelLauncherExtended.zip"
 fi
-
-if [ ! -f setup.sh ]; then
-    echo "setup.sh not found, are you sure that you are in the right directory?"
-    exit
-fi
-
-echo ">> Creating zip file"
-echo ""
-zip -r PixelLauncherExtended.zip . -x .git/\*
-
-echo ""
-echo ">> Done! You can find the zip file in the current directory - $(pwd)/PixelLauncherExtended.zip"
