fa6cd950b43e2fa69aca0fa4ff89ea72d7f7c537
saitamasahil <sahilmanchanda1996@gmail.com>
2022-12-05 15:48:28 +0530

    added 7z 32 bit support

--- win.bat
+++ win.bat
@@ -3,7 +3,14 @@ setlocal
 
 echo ">> Creating zip file"
 
-"C:\Program Files\7-Zip\7z.exe" a -tzip PixelLauncherExtended.zip * -x!.git\*
+if exist "C:\Program Files\7-Zip\7z.exe" (
+  "C:\Program Files\7-Zip\7z.exe" a -tzip PixelLauncherExtended.zip * -x!.git\*
+) else if exist "C:\Program Files (x86)\7-Zip\7z.exe" (
+  "C:\Tools\7z.exe" a -tzip PixelLauncherExtended.zip * -x!.git\*
+) else (
+  echo "ERROR: 7zip executable not found!"
+  exit /b 1
+)
 
 echo ""
 echo ">> Done! You can find the zip file in the current directory - '%CD%/PixelLauncherExtended.zip'"
