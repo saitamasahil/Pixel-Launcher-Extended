ab2956f1951493c71faf909b755f2ebbaf39d1c9
Devang Chaudhary <devangsingh369@gmail.com>
2022-12-05 11:18:13 +0530

    autobuild.sh: Add a `Package Manager` check to verify the `Distro Type`
    * This checks if the distro is Arch based or Debian and runs the script accordingly.
    
    Signed-off-by: Devang Chaudhary <devangsingh369@gmail.com>

--- autobuild.sh
+++ autobuild.sh
@@ -1,24 +1,37 @@
 #!/usr/bin/bash
-# Setup script
 
-# Install requirements
-sudo apt-get install zip
+# Check the Distro Variant
 
-# Make autobuild.sh executable
+which "apt"
+
+RESULT=$?
+
+# Install Requirements
+
+if [ $RESULT -eq 0 ]; then
+    sudo apt-get install zip
+else
+    sudo pacman -S zip
+
+fi
+
+# Make the Script Executable
+
 chmod +x autobuild.sh
 
-# Give permission
-sudo ./autobuild.sh
-
 # Check if the Pixel-Launcher-Extended directory exists and contains files
+
 if [ ! -d Pixel-Launcher-Extended ] || [ -z "$(ls -A Pixel-Launcher-Extended)" ]; then
     echo ">> Pixel-Launcher-Extended directory not found or is empty"
     exit
 else
+    
     # Make sure the script has the correct permissions to access the directory
+    
     chmod u+rwx Pixel-Launcher-Extended
 
     # Create zip file
+    
     echo ">> Creating zip file"
     zip -r PixelLauncherExtended.zip Pixel-Launcher-Extended
     echo ">> Done! You can find the zip file in the current directory - $(pwd)/PixelLauncherExtended.zip"
