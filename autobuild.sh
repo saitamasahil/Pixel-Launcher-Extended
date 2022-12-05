837cdb08a0172a5579451d5c1bb10230004142b8
saitamasahil <100558482+saitamasahil@users.noreply.github.com>
2022-12-05 12:45:15 +0530

    added cd /tmp

--- autobuild.sh
+++ autobuild.sh
@@ -13,7 +13,10 @@ mkdir -p "$HOME/Pixel-Launcher-Extended"
 # Make sure the script has the correct permissions to access the directory
 chmod a+rwx "$HOME/Pixel-Launcher-Extended"
 
+# Change to the /tmp directory
+cd /tmp
+
 # Create zip file
 echo ">> Creating zip file"
 zip -r PixelLauncherExtended.zip "$HOME/Pixel-Launcher-Extended"
-echo ">> Done! You can find the zip file in the current directory - $(pwd)/PixelLauncherExtended.zip"
+echo ">> Done! You can find the zip file in the /tmp directory - /tmp/PixelLauncherExtended.zip"
