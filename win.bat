75a7d0fa343db0d8c37c4f6c49a01c3798f925cf
saitamasahil <sahilmanchanda1996@gmail.com>
2022-12-05 15:25:27 +0530

    added 7zip path

--- win.bat
+++ win.bat
@@ -3,7 +3,7 @@ setlocal
 
 echo ">> Creating zip file"
 
-7za a -tzip PixelLauncherExtended.zip * -x!.git\*
+"C:\Program Files\7-Zip\7z.exe" a -tzip PixelLauncherExtended.zip * -x!.git\*
 
 echo ""
 echo ">> Done! You can find the zip file in the current directory - '%CD%/PixelLauncherExtended.zip'"
