a6bd1c8f8afc2613e51aa81c6d149931d3955114
saitamasahil <sahilmanchanda1996@gmail.com>
2022-12-15 21:24:45 +0530

    added more distro type

--- autobuild.sh
+++ autobuild.sh
@@ -2,17 +2,53 @@
 
 # Check for the Distro Type
 
+# Check if apt package manager is available
 which "apt"
-
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
+  # Install zip package using apt-get
   sudo apt-get install zip
 fi
 
-if [ $RESULT -eq 1 ]; then
+# Check if yum package manager is available
+which "yum"
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  # Install zip package using yum
+  sudo yum install zip
+fi
+
+# Check if dnf package manager is available
+which "dnf"
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  # Install zip package using dnf
+  sudo dnf install zip
+fi
+
+# Check if pacman package manager is available
+which "pacman"
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  # Install zip package using pacman
   sudo pacman -S zip
 fi
 
+# Check if zypper package manager is available
+which "zypper"
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  # Install zip package using zypper
+  sudo zypper install zip
+fi
+
+# Check if the current Linux distribution is Fedora
+if [ -f /etc/fedora-release ]; then
+  # Install zip package using dnf
+  sudo dnf install zip
+fi
+
+# Check if zip is installed
 if ! command -v zip >/dev/null; then
   echo "Error: zip is not installed. Please install it manually and try again."
   exit 1
