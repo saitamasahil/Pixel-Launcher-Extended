343090fec829d05d9967e0d667c823cc61464dc9
saitamasahil <sahilmanchanda1996@gmail.com>
2022-12-16 20:10:41 +0530

    noob mistakes 😑

--- autobuild.bat
+++ autobuild.bat
@@ -6,7 +6,7 @@ echo >> Creating zip file
 if exist "C:\Program Files\7-Zip\7z.exe" (
   "C:\Program Files\7-Zip\7z.exe" a -tzip PixelLauncherExtended.zip * -x!.git\* -x!ThemedIcons\* -x!screenshots\* -x!autobuild.bat -x!banner.jpg -x!banner2.jpg -x!codename.txt -x!logo.png -x!online_installer_setup.sh -x!README.md
 ) else if exist "C:\Program Files (x86)\7-Zip\7z.exe" (
-  "C:\Tools\7z.exe" a -tzip PixelLauncherExtended.zip * -x!.git\* -x!ThemedIcons\* -x!screenshots\* -x!autobuild.bat -x!banner.jpg -x!Banner2.jpg -x!codename.txt -x!logo.png -x!online installer setup.sh -x!README.md
+  "C:\Tools\7z.exe" a -tzip PixelLauncherExtended.zip * -x!.git\* -x!ThemedIcons\* -x!screenshots\* -x!autobuild.bat -x!banner.jpg -x!banner2.jpg -x!codename.txt -x!logo.png -x!online_installer_setup.sh -x!README.md
 ) else (
   echo ERROR: 7zip executable not found! Please install it from https://www.7-zip.org/download.html
   exit /b 1
