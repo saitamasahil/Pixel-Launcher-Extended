171ec7f741b71d8a36550e2645dc09e36a7d2bdf
saitamasahil <sahilmanchanda1996@gmail.com>
2022-12-17 22:47:25 +0530

    removed more clutter in output module in win batch

--- autobuild.bat
+++ autobuild.bat
@@ -10,9 +10,9 @@ ren offline_setup.sh setup.sh
 echo ^>^> Creating zip file
 
 if exist "C:\Program Files\7-Zip\7z.exe" (
-  "C:\Program Files\7-Zip\7z.exe" a -tzip "Pixel Launcher Extended Version %version%.zip" * -x!.git\* -x!ThemedIcons\* -x!screenshots\* -x!autobuild.bat -x!banner.jpg -x!banner2.jpg -x!changelog.md -x!codename.txt -x!logo.png -x!online_setup.sh -x!README.md
+  "C:\Program Files\7-Zip\7z.exe" a -tzip "Pixel Launcher Extended Version %version%.zip" * -x!.git -x!ThemedIcons -x!screenshots -x!autobuild.bat -x!autobuild.sh -x!banner.jpg -x!banner2.jpg -x!changelog.md -x!codename.txt -x!logo.png -x!online_setup.sh -x!README.md
 ) else if exist "C:\Program Files (x86)\7-Zip\7z.exe" (
-  "C:\Tools\7z.exe" a -tzip "Pixel Launcher Extended Version %version%.zip" * -x!.git\* -x!ThemedIcons\* -x!screenshots\* -x!autobuild.bat -x!banner.jpg -x!banner2.jpg -x!changelog.md -x!codename.txt -x!logo.png -x!online_setup.sh -x!README.md
+  "C:\Tools\7z.exe" a -tzip "Pixel Launcher Extended Version %version%.zip" * -x!.git -x!ThemedIcons -x!screenshots -x!autobuild.bat -x!autobuild.sh -x!banner.jpg -x!banner2.jpg -x!changelog.md -x!codename.txt -x!logo.png -x!online_setup.sh -x!README.md
 ) else (
   echo ERROR^: 7zip executable not found! Please install it from https://www.7-zip.org/download.html
   exit /b 1
