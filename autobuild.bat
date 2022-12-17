5d9ec057a40a978d5827ca8d96a477be4f28f1c7
saitamasahil <sahilmanchanda1996@gmail.com>
2022-12-17 21:59:20 +0530

    initiated change to support automated online installer

--- autobuild.bat
+++ autobuild.bat
@@ -4,12 +4,15 @@ setlocal
 @REM Read the version details from the module.prop file
 for /f "tokens=2 delims=<> " %%a in ('find "version=Version" ^< "module.prop" ') do set version=%%a
 
+echo ^>^> Renaming offline_setup.sh to setup.sh
+ren offline_setup.sh setup.sh
+
 echo ^>^> Creating zip file
 
 if exist "C:\Program Files\7-Zip\7z.exe" (
-  "C:\Program Files\7-Zip\7z.exe" a -tzip "Pixel Launcher Extended Version %version%.zip" * -x!.git\* -x!ThemedIcons\* -x!screenshots\* -x!autobuild.bat -x!banner.jpg -x!banner2.jpg -x!codename.txt -x!logo.png -x!online_installer_setup.sh -x!README.md
+  "C:\Program Files\7-Zip\7z.exe" a -tzip "Pixel Launcher Extended Version %version%.zip" * -x!.git\* -x!ThemedIcons\* -x!screenshots\* -x!autobuild.bat -x!banner.jpg -x!banner2.jpg -x!changelog.md -x!codename.txt -x!logo.png -x!online_setup.sh -x!README.md
 ) else if exist "C:\Program Files (x86)\7-Zip\7z.exe" (
-  "C:\Tools\7z.exe" a -tzip "Pixel Launcher Extended Version %version%.zip" * -x!.git\* -x!ThemedIcons\* -x!screenshots\* -x!autobuild.bat -x!banner.jpg -x!banner2.jpg -x!codename.txt -x!logo.png -x!online_installer_setup.sh -x!README.md
+  "C:\Tools\7z.exe" a -tzip "Pixel Launcher Extended Version %version%.zip" * -x!.git\* -x!ThemedIcons\* -x!screenshots\* -x!autobuild.bat -x!banner.jpg -x!banner2.jpg -x!changelog.md -x!codename.txt -x!logo.png -x!online_setup.sh -x!README.md
 ) else (
   echo ERROR^: 7zip executable not found! Please install it from https://www.7-zip.org/download.html
   exit /b 1
--- autobuild.sh
+++ autobuild.sh
@@ -54,6 +54,9 @@ if ! command -v zip >/dev/null; then
   exit 1
 fi
 
+# Rename offline_setup.sh to setup.sh
+mv offline_setup.sh setup.sh
+
 # Check if the current directory has system folder and setup.sh to verify that current directory is valid
 if [ ! -d "system" ] || [ ! -f "setup.sh" ]; then
   echo "Error: Current directory is not valid. Make sure that you are in the right directory and try again."
@@ -66,6 +69,6 @@ version=$(grep "version=" module.prop | cut -d "=" -f 2)
 # Create zip file
 echo ">> Creating zip file"
 echo "" # make the output look easier to read
-zip -r "Pixel Launcher Extended $version.zip" . -x .git/\* ThemedIcons/\* screenshots/\* autobuild.sh autobuild.bat banner.jpg banner2.jpg codename.txt logo.png online_installer_setup.sh README.md # Ignore specified files and folders because they are not needed for the module
+zip -r "Pixel Launcher Extended $version.zip" . -x .git/\* ThemedIcons/\* screenshots/\* autobuild.sh autobuild.bat banner.jpg banner2.jpg changelog.md codename.txt logo.png online_setup.sh README.md # Ignore specified files and folders because they are not needed for the module
 echo "" # make the output look easier to read
 echo ">> Done! You can find the zip file in the current directory - '$(pwd)/Pixel Launcher Extended $version.zip'"
