12452a3f351f1e43adcad2c834d7d4834e952143
saitamasahil <sahilmanchanda1996@gmail.com>
2022-12-28 12:53:27 +0530

    more magisk script improvements
    added push notication service choice. now users will be able to choose if they want updates & support related notifications or not.

--- offline_setup.sh
+++ offline_setup.sh
@@ -929,6 +929,51 @@ init_main() {
     rm -rf "$MODPATH/system/product/overlay/IconShape/Vessel.apk"
   fi
 
+  ui_print ""
+  ui_print "[*] Do you want to add push notification service?"
+  ui_print "[*] We will send notifications for quick support"
+  ui_print "or if there is any new update available"
+  ui_print "[*] This feature uses very little internet"
+  ui_print "[*] Check ReadMe on GitHub to know more about it"
+  ui_print "[*] Press volume up to switch to another choice"
+  ui_print "[*] Press volume down to continue with that choice"
+  ui_print ""
+
+  sleep 0.5
+
+  ui_print "--------------------------------"
+  ui_print "[1] Yes"
+  ui_print "--------------------------------"
+  ui_print "[2] NO"
+  ui_print "--------------------------------"
+
+  ui_print ""
+  ui_print "[*] Select your desired option:"
+
+  SM=1
+  while true; do
+    ui_print "  $SM"
+    "$VKSEL" && SM="$((SM + 1))" || break
+    [[ "$SM" -gt "2" ]] && SM=1
+  done
+
+  case "$SM" in
+  "1") FCTEXTAD1="Yes" ;;
+  "2") FCTEXTAD1="No" ;;
+  esac
+
+  ui_print "[*] Selected: $FCTEXTAD1"
+  ui_print ""
+
+  if [[ "$FCTEXTAD1" == "Yes" ]]; then
+
+  elif [[ "$FCTEXTAD1" == "No" ]]; then
+    rm -rf "$MODPATH/system/lib64"
+    rm -rf "$MODPATH/CNS"
+    rm -rf "$MODPATH/system/product/priv-app/OnePunchNotifier"
+    rm -rf "$MODPATH/system/product/etc/permissions/privapp-permissions-com.looper.notifier.xml"
+  fi
+
   ui_print "[*] Clearing system cache to make it work properly..."
   ui_print ""
 
