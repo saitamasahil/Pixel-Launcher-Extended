a619ac602c844e6fc8b575f63ae1fd4f37522bdf
saitamasahil <sahilmanchanda1996@gmail.com>
2023-02-22 19:00:49 +0530

    improve script to detect & download required pkgs

--- autobuild.sh
+++ autobuild.sh
@@ -2,58 +2,25 @@
 
 # Check for the Distro Type
 
-# Check if pkg package manager is available
-which "pkg"
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-  # Install zip and figlet package using pkg
-  pkg install zip figlet
-fi
+PACKAGE_MANAGERS=("pkg" "apt" "yum" "dnf" "pacman" "zypper")
+PACKAGES=("zip" "figlet")
 
-# Check if apt package manager is available
-which "apt"
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-  # Install zip and figlet package using apt-get
-  sudo apt-get install zip figlet
-fi
+for PM in "${PACKAGE_MANAGERS[@]}"; do
+  if command -v "$PM" > /dev/null; then
+    case "$PM" in
+      "pkg") pkg install "${PACKAGES[@]}" ;;
+      "apt") sudo apt-get install "${PACKAGES[@]}" ;;
+      "yum") sudo yum install "${PACKAGES[@]}" ;;
+      "dnf") sudo dnf install "${PACKAGES[@]}" ;;
+      "pacman") sudo pacman -S "${PACKAGES[@]}" ;;
+      "zypper") sudo zypper install "${PACKAGES[@]}" ;;
+    esac
+    break
+  fi
+done
 
-# Check if yum package manager is available
-which "yum"
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-  # Install zip and figlet package using yum
-  sudo yum install zip figlet
-fi
-
-# Check if dnf package manager is available
-which "dnf"
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-  # Install zip and figlet package using dnf
-  sudo dnf install zip figlet
-fi
-
-# Check if pacman package manager is available
-which "pacman"
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-  # Install zip and figlet package using pacman
-  sudo pacman -S zip figlet
-fi
-
-# Check if zypper package manager is available
-which "zypper"
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-  # Install zip and figlet package using zypper
-  sudo zypper install zip figlet
-fi
-
-# Check if the current Linux distribution is Fedora
 if [ -f /etc/fedora-release ]; then
-  # Install zip and figlet package using dnf
-  sudo dnf install zip figlet
+  sudo dnf install "${PACKAGES[@]}"
 fi
 
 # Display "PLE Builder" in bigger fonts
